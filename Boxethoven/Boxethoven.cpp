a2ed0e454d7bdf57608723787597d23685e96b56
PeterSteve <pedropote677@gmail.com>
2025-07-07 13:32:57 -0300

    Adicionar arquivos de projeto.

---
+++ Boxethoven/Boxethoven.cpp
@@ -0,0 +1,12 @@
+﻿// Boxethoven.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "Boxethoven.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
